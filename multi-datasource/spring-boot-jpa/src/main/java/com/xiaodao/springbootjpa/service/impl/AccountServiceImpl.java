15a7a2cc110186ec76e02a8a830997405e1f9373
xiaodaojiang <xiaodaojiang@aliyun.com>
2024-04-15 22:23:24 +0800

    chore: modify some comment

--- multi-datasource/spring-boot-jpa/src/main/java/com/xiaodao/springbootjpa/service/impl/AccountServiceImpl.java
+++ multi-datasource/spring-boot-jpa/src/main/java/com/xiaodao/springbootjpa/service/impl/AccountServiceImpl.java
@@ -37,7 +37,10 @@ public class AccountServiceImpl implements AccountService {
     // 默认的事务传播行为是 Propagation.REQUIRED，即如果当前没有事务，就新建一个事务，如果已经存在一个事务中，加入到这个事务中。
     // 默认的隔离级别是 Isolation.DEFAULT，表示使用底层数据库的默认隔离级别。
     // @Transactional(propagation = Propagation.REQUIRED,isolation = Isolation.DEFAULT)
+
+    // 新建一个独立的事务运行，如果当前存在事务，则把当前事务挂起。当前事务运行后，挂起的事务继续执行。
     @Transactional(propagation = Propagation.REQUIRES_NEW,isolation = Isolation.DEFAULT)
+    // @Transactional(propagation = Propagation.NESTED,isolation = Isolation.DEFAULT)
     public boolean checkExist(String username) {
         return accountRepository.existsByUsername(username);
     }
