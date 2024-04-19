6c560587506a6f892544272a046931600cb43abc
xiaodaojiang <xiaodaojiang@aliyun.com>
2024-04-19 19:27:45 +0800

    feat(misc): :sparkles: graphql demo

---
+++ misc/graph-spqr-demo/src/main/java/com/xiaodao/graphspqrtest/GraphSpqrDemoApplication.java
@@ -0,0 +1,13 @@
+package com.xiaodao.graphspqrtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphSpqrDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphSpqrDemoApplication.class, args);
+	}
+
+}
---
+++ misc/graph-spqr-demo/src/test/java/com/xiaodao/graphspqrtest/GraphSpqrTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaodao.graphspqrtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphSpqrTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
