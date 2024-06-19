373e84de0309230f5778e3110df5d981118266cb
xiaodaojiang <xiaodaojiang@aliyun.com>
2024-06-19 09:12:44 +0800

    test(): :white_check_mark: test request body map

--- web/src/main/java/com/xiaodao/controller/HelloController.java
+++ web/src/main/java/com/xiaodao/controller/HelloController.java
@@ -3,10 +3,9 @@ package com.xiaodao.controller;
 import com.xiaodao.CommonResult;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -29,8 +28,16 @@ public class HelloController {
         return CommonResult.success(user);
     }
 
+    @PostMapping("map")
+    private CommonResult<HelloController.User> testMap(@RequestBody User user) {
+        log.info("hello map：{}", user);
+        return CommonResult.success(user);
+    }
+
     @Data
     public static class User {
         private String name;
+
+        private Map<String,Object> properties;
     }
 }
