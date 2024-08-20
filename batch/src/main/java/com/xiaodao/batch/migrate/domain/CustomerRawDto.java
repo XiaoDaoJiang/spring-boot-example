6fce47bcbf8a8bd3ace3bcc02ea1bb1bdb247850
xiaodaojiang <xiaodaojiang@aliyun.com>
2024-08-20 19:09:04 +0800

    batch :safety_vest:

--- batch/src/main/java/com/xiaodao/batch/migrate/domain/CustomerRawDto.java
+++ batch/src/main/java/com/xiaodao/batch/migrate/domain/CustomerRawDto.java
@@ -3,6 +3,11 @@ package com.xiaodao.batch.migrate.domain;
 import com.alibaba.excel.annotation.ExcelProperty;
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 /**
  * @author jianghaitao
  * @Classname CustomerRawDto
@@ -13,15 +18,21 @@ import lombok.Data;
 @Data
 public class CustomerRawDto {
 
+    @NotBlank
     @ExcelProperty("顾客")
     private String customerName;
 
+    @NotBlank
     @ExcelProperty("订单子项名")
     private String name;
 
+    @NotNull
+    @Max(value = 200)
+    @Min(value = 1)
     @ExcelProperty("单个订单购买数")
     private Long quantity;
 
+    @Min(value = 1)
     @ExcelProperty("库存数量")
     private long stockNumber;
 
