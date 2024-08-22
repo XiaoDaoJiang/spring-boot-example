8727a1772a99c859ec476289b06f4ab01d3528a2
xiaodaojiang <xiaodaojiang@aliyun.com>
2024-08-22 23:33:19 +0800

    :fire: batch :safety_vest:

--- batch/src/main/java/com/xiaodao/batch/migrate/support/ValidationRetainingItemProcessor.java
+++ batch/src/main/java/com/xiaodao/batch/migrate/support/ValidationRetainingItemProcessor.java
@@ -1,5 +1,8 @@
 package com.xiaodao.batch.migrate.support;
 
+import lombok.Data;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.annotation.BeforeStep;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.validator.BeanValidatingItemProcessor;
 import org.springframework.batch.item.validator.ValidationException;
@@ -9,6 +12,8 @@ import javax.validation.ConstraintViolationException;
 public class ValidationRetainingItemProcessor<T> implements ItemProcessor<T, ValidationResult<T>> {
 
     private final BeanValidatingItemProcessor<T> delegate;
+
+    private final ValidationContext validationContext = new ValidationContext();
 
     public ValidationRetainingItemProcessor() {
         this.delegate = new BeanValidatingItemProcessor<>();
@@ -22,10 +27,14 @@ public class ValidationRetainingItemProcessor<T> implements ItemProcessor<T, Val
 
     @Override
     public ValidationResult<T> process(T item) {
+        this.validationContext.totalCount++;
         ValidationResult<T> result = new ValidationResult<>(item);
         try {
             delegate.process(item); // 进行校验
+            this.validationContext.successCount++;
         } catch (ValidationException e) {
+            this.validationContext.invalidCount++;
+            result.setError(true);
             if (e.getCause() instanceof ConstraintViolationException) {
                 ConstraintViolationException violationException = (ConstraintViolationException) e.getCause();
                 result.setViolationException(violationException); // 保存校验异常信息
@@ -35,4 +44,17 @@ public class ValidationRetainingItemProcessor<T> implements ItemProcessor<T, Val
         }
         return result; // 返回校验结果对象
     }
+
+    @BeforeStep
+    public void initValidStepContext(StepExecution stepExecution) {
+        stepExecution.getExecutionContext().put("validationContext", this.validationContext);
+    }
+
+    @Data
+    public static class ValidationContext {
+        private int totalCount;
+        private int successCount;
+        private int invalidCount;
+    }
+
 }
