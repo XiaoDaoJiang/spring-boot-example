3850a2da1de94158eee57b6d9171b7f273938f42
XiaoDaoJiang <xiaodaojiang@aliyun.com>
2025-04-25 16:15:17 +0800

    feat(): :sparkles: add graalvm hello world[/]

---
+++ graalvm/src/test/java/com/xiaodao/graalvm/GraalvmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaodao.graalvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalvmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
